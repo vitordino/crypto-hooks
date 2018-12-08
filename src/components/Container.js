20b183d03674ca37c0cff1ea9f1a6afb0562de50
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-08 08:55:09 -0200

    :sparkles: add Container

---
+++ src/components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	width: 100%;
+	max-width: 54rem;
+	padding: 0 1rem;
+`
+
+export default Container
