e284af673f8fdcbed83e8a1af16ccc023d83b191
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-08 05:39:26 -0200

    :chart_with_downwards_trend: btc victory CandleStick

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { useFetch } from 'react-hooks-fetch'
-import Trend from 'react-trend'
+import { VictoryChart , VictoryAxis, VictoryCandlestick } from 'victory'
 
 const Err = ({ error }) => <span>Error: {error.message}</span>
 const Loading = () => <span>Loading...</span>
 
 const App = () => {
-	const { error, loading, data } = useFetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=365')
+	const { error, loading, data } = useFetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=182')
 	if (error) return <Err error={error} />
 	if (loading) return <Loading />
 	if (data.Type !== 100) return <Err error={{message: 'api error'}}/>
+
+	const normalizedData = data.Data.map(({time, ...props}) => (
+		({x: new Date(time*1000), ...props}))
+	)
+
 	return (
 		<div>
-			<h1>bitcoin price since last year</h1>
-			<Trend
-				smooth
-				autoDraw
-				data={data.Data.map(({low, high}) => (low+high))}
-				gradient={['#e64a19', '#b2ff59']}
-				strokeLinecap={'round'}
-			/>
+			<h1>bitcoin price last semester</h1>
+			<VictoryChart scale={{ x: "time" }}>
+				<VictoryAxis tickFormat={(t) => t.toLocaleDateString('en-US', {month: 'short'})}/>
+				<VictoryAxis dependentAxis/>
+				<VictoryCandlestick
+					data={normalizedData}
+				/>
+			</VictoryChart>
 		</div>
 	)
 }
