427a2c5827f4b258438582167ffec287b1227dd8
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-08 05:06:01 -0200

    :chart_with_downwards_trend: btc react-trend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { useFetch } from 'react-hooks-fetch'
+import Trend from 'react-trend'
 
 const Err = ({ error }) => <span>Error: {error.message}</span>
 const Loading = () => <span>Loading...</span>
 
 const App = () => {
-	const { error, loading, data } = useFetch('https://jsonplaceholder.typicode.com/todos')
+	const { error, loading, data } = useFetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=365')
 	if (error) return <Err error={error} />
 	if (loading) return <Loading />
+	if (data.Type !== 100) return <Err error={{message: 'api error'}}/>
 	return (
-		<span>
-			{data.map(x => <div>{x.id}: {x.title}</div>)}
-		</span>
+		<div>
+			<h1>bitcoin price since last year</h1>
+			<Trend
+				smooth
+				autoDraw
+				data={data.Data.map(({low, high}) => (low+high))}
+				gradient={['#e64a19', '#b2ff59']}
+				strokeLinecap={'round'}
+			/>
+		</div>
 	)
 }
 
