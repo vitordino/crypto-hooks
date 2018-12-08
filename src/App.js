032fa69fd96398aef0446eba8986cf7b69db3263
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-08 09:29:12 -0200

    :art: more base props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { useFetch } from 'react-hooks-fetch'
+import Container from './components/Container'
 import Card from './components/Card'
 
 const Err = ({ error }) => <span>Error: {error.message}</span>
 const Loading = () => <span>Loading...</span>
 
-const App = () => {
-	const { error, loading, data } = useFetch('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=182')
+const App = ({currency = 'BTC', fiat = 'USD', limit = 30}) => {
+	const { error, loading, data } = useFetch(`https://min-api.cryptocompare.com/data/histoday?fsym=${currency}&tsym=${fiat}&limit=${limit}`)
 	if (error) return <Err error={error} />
 	if (loading) return <Loading />
 	if (data.Type !== 100) return <Err error={{message: 'api error'}}/>
 	return (
-		<div>
-			<h1>bitcoin price last semester</h1>
-			<Card data={data.Data}/>
-		</div>
+		<Container>
+			<h1>{currency} price last semester</h1>
+			<Card
+				data={data.Data}
+				currency={currency}
+				fiat={fiat}
+			/>
+		</Container>
 	)
 }
 
